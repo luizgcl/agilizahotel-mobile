e34588cf7008533fbbf1c6e3794926cf10d3aedb
luizgcl <luiizgcl@gmail.com>
2023-03-03 20:29:44 -0300

    chore: adicionando background nos inputs

--- components/GeneralInput/style.js
+++ components/GeneralInput/style.js
@@ -3,9 +3,10 @@ import { StyleSheet } from 'react-native';
 export const style = StyleSheet.create({
     input: {
         width: 250,
-        padding: 6,
-        margin: 5,
+        padding: 8,
+        margin: 6,
         borderRadius: 8,
+        backgroundColor: '#f2f2f2',
         borderColor: '#2a2a2a',
         borderWidth: 1,
     }
